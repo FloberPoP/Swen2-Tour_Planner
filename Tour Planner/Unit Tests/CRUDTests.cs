403dadd0c8df3fa57f2058d1e6e1f7cd85300106
Florian Poppinger <florianpoppinger@gmail.com>
2024-03-26 14:34:44 +0100

    Unit Tests

---
+++ Tour Planner/Unit Tests/CRUDTests.cs
@@ -0,0 +1,90 @@
+﻿using Tour_Planner;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TourPlannerVMTests
+    {
+        private TourPlannerVM tourPlannerVM;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            tourPlannerVM = new TourPlannerVM();
+        }
+
+        [TestMethod]
+        public void AddTour()
+        {
+            int initialCount = tourPlannerVM.Tours.Count;
+
+            tourPlannerVM.AddTour();
+
+            Assert.AreEqual(initialCount + 1, tourPlannerVM.Tours.Count);
+        }
+
+        [TestMethod]
+        public void UpdateTour()
+        {
+            var selectedTour = new Tour { Name = "TestTour" };
+
+            tourPlannerVM.SelectedTour = selectedTour;
+            tourPlannerVM.UpdateTour();
+
+            Assert.AreEqual(tourPlannerVM.NewTourName, selectedTour.Name);
+        }
+
+        [TestMethod]
+        public void DeleteTour()
+        {
+            var tourToRemove = new Tour { Name = "TestTour" };
+
+            tourPlannerVM.Tours.Add(tourToRemove);
+            tourPlannerVM.SelectedTour = tourToRemove;
+            tourPlannerVM.DeleteTour();
+
+            Assert.IsFalse(tourPlannerVM.Tours.Contains(tourToRemove));
+        }
+
+        [TestMethod]
+        public void AddTourLog()
+        {
+            var selectedTour = new Tour { Name = "TestTour" };
+            tourPlannerVM.Tours.Add(selectedTour);
+            tourPlannerVM.TourLogsSelectedTour = selectedTour;
+            int initialCount = selectedTour.TourLogs.Count;
+
+            tourPlannerVM.AddTourLog();
+
+            Assert.AreEqual(initialCount + 1, selectedTour.TourLogs.Count);
+        }
+
+        [TestMethod]
+        public void UpdateTourLog()
+        {
+            var selectedTour = new Tour { Name = "TestTour" };
+            var selectedTourLog = new TourLog { tour = selectedTour, DateTime = DateTime.Now, Comment = "TestComment" };
+            tourPlannerVM.TourLogsSelectedTour = selectedTour;
+            tourPlannerVM.SelectedTourLog = selectedTourLog;
+
+            tourPlannerVM.SaveTourLog();
+
+            Assert.AreEqual(selectedTourLog.DateTime, tourPlannerVM.NewDateTime);
+            Assert.AreEqual(selectedTourLog.Comment, tourPlannerVM.NewComment);
+        }
+
+        [TestMethod]
+        public void DeleteTourLog()
+        {
+            var selectedTour = new Tour { Name = "TestTour" };
+            var tourLogToDelete = new TourLog { tour = selectedTour, DateTime = DateTime.Now, Comment = "TestComment" };
+            tourPlannerVM.TourLogsSelectedTour = selectedTour;
+            tourPlannerVM.SelectedTourLog = tourLogToDelete;
+            tourPlannerVM.AddTourLog();
+
+            tourPlannerVM.DeleteTourLog();
+
+            Assert.IsFalse(tourPlannerVM.TourLogsSelectedTour.TourLogs.Contains(tourLogToDelete));
+        }
+    }
+}
