a714218e3a2e2cefe88af508896df56baf61f66e
Tobias Kastl <if22b007@technikum-wien.at>
2024-05-30 16:14:56 +0200

    update
    changing from laptop to PC, i hoids nimma aus

--- Tour Planner/Tour Planner/BL/RouteService.cs
+++ Tour Planner/Tour Planner/BL/RouteService.cs
@@ -1,25 +1,73 @@
 ﻿using System.Net.Http;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Tour_Planner.Models;
 
 namespace Tour_Planner.BL
 {
     public class RouteService
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey;
+        private readonly string _apiKey = "5b3ce3597851110001cf6248d157c6099b64438e81765a43f9ecc90b";
+        private readonly string _urlSearchGeocode = "https://api.openrouteservice.org/geocode/search";
+        private readonly string _urlDirections = "https://api.openrouteservice.org/v2/directions/";
 
-        public RouteService(HttpClient httpClient, string apiKey)
+        public RouteService()
         {
-            _httpClient = httpClient;
-            _apiKey = apiKey;
+            _httpClient = new HttpClient();
         }
 
-        public JObject GetRoute(string from, string to)
+        static Dictionary<string, string> TransportDictionary = new()
         {
-            var response = _httpClient.GetAsync($"https://api.openrouteservice.org/v2/directions/driving-car?api_key={_apiKey}&start={from}&end={to}").Result;
-            response.EnsureSuccessStatusCode();
-            var content = response.Content.ReadAsStringAsync().Result;
-            return JObject.Parse(content);
+            {"Walk", "foot-walking"},
+            {"Bike", "cycling-regular"},
+            {"Car", "driving-car"}
+        };
+
+        /*
+        public async Task<(double Distance, double Duration, string MapImageUrl)> GetRouteInfo(string address1, string address2, string transportation)
+        {
+            try
+            {
+                var locationA = await GetGeoAddress(address1);
+                var locationB = await GetGeoAddress(address2);
+            }
+
+            catch
+            {
+
+            }
+        }
+        */
+
+        private async Task<List<double>> GetGeoAddress(string address)
+        {
+            try
+            {
+                string requestUrl = $"{_urlSearchGeocode}?api_key={_apiKey}&text={Uri.EscapeDataString(address)}";
+                //logger.Info($"Requesting geocode with URL: {requestUrl}");
+
+                HttpResponseMessage response = await _httpClient.GetAsync(requestUrl);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var geocodeData = JsonConvert.DeserializeObject<GeoSearchResponse>(responseBody);
+
+                if (geocodeData?.Features == null || geocodeData.Features.Count == 0)
+                {
+                    //logger.Error($"No geocode data found for address: {address}");
+                    return null;
+                }
+
+                return geocodeData.Features[0].Geometry.Coordinates;
+            }
+
+            catch (Exception e)
+            {
+                //logger.Error($"An error occurred while geocoding address: {e.Message}");
+                return null;
+            }
         }
     }
 }
---
+++ Tour Planner/Tour Planner/Models/GeoSearchResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour_Planner.Models
+{
+    public class GeoSearchResponse
+    {
+        [JsonProperty("features")]
+        public List<GeocodeFeature> Features { get; set; }
+
+        public class GeocodeFeature
+        {
+            [JsonProperty("geometry")]
+            public Geometry Geometry { get; set; }
+        }
+
+        public class Geometry
+        {
+            [JsonProperty("coordinates")]
+            public List<double> Coordinates { get; set; }
+        }
+    }
+}
